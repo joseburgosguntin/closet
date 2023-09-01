c2fae51a551b77ea47ba6c708e1aeb2ce1b8fe6d
joseburgosguntin <josem.123burgo@gmail.com>
2023-09-01 17:23:20 -0400

    combo svg template, rand combo, favicon

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod axum_htmx;
 use askama::Template;
 use axum::{
     extract::State,
-    response::{Html, IntoResponse},
+    response::{Html, IntoResponse, Redirect},
     routing::post,
+    Json,
 };
 use axum::{handler::HandlerWithoutStateExt, http::StatusCode, routing::get, Router};
 use axum_htmx::HtmxPostRequest;
 use serde::Deserialize;
+use serde::Serialize;
+use shuttle_runtime::tracing::info;
 use sqlx::{FromRow, PgPool};
 use std::{fmt::Display, path::PathBuf, sync::Arc};
 use tower_http::services::ServeDir;
@@ -27,129 +30,209 @@ impl Display for Rating {
     }
 }
 
-#[derive(Deserialize, Debug, Template)]
-#[template(path = "combo.html")]
+#[derive(Deserialize)]
 struct RatedCombo {
     rating: Rating,
-    shirt_id: i64,
-    short_id: i64,
-    weight: f64,
+    shirt_color: i32,
+    short_color: i32,
 }
 
-#[derive(Debug, Clone, Deserialize, FromRow)]
-struct WeightedCombo {
-    shirt_id: i64,
-    short_id: i64,
-    weight: f64,
+#[derive(Template)]
+#[template(path = "combo.html")]
+struct Combo {
+    shirt_color: i32,
+    short_color: i32,
 }
 
-#[derive(Debug, FromRow)]
-struct SvgWithId {
-    id: i64,
-    svg: String,
+#[derive(FromRow, Debug)]
+struct WeightedShort {
+    short_color: i32,
+    weight: f32,
 }
 
-#[derive(FromRow)]
-struct ClotheId {
-    id: i64,
+#[derive(FromRow, Serialize, Clone, Debug)]
+struct ClotheColor {
+    color: i32,
 }
 
-async fn user_id_and_days() -> (Uuid, u32) {
-    todo!("get user from cookies and days from redis")
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error(transparent)]
+    Render(#[from] askama::Error),
+    #[error(transparent)]
+    Sqlx(#[from] sqlx::Error),
+    #[error("failed to get user id and days")]
+    UserIdAndDays,
+    #[error("failed to find any {0}")]
+    EmptyClothes(&'static str),
+    #[error(transparent)]
+    Join(#[from] tokio::task::JoinError),
+    #[error(transparent)]
+    Uuid(#[from] uuid::Error),
 }
 
-async fn clothes(
-    pool: &PgPool,
-    clothes: &'static str,
-    user_id: Uuid,
-    days: u32,
-) -> sqlx::Result<Vec<ClotheId>> {
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        use Error::*;
+        match self {
+            Render(_) | Sqlx(_) | Join(_) | Uuid(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+            }
+            UserIdAndDays => Redirect::permanent("/login").into_response(),
+            EmptyClothes(_) => Redirect::permanent("/closet").into_response(),
+        }
+    }
+}
+
+async fn user_id_and_days() -> Option<(Uuid, i32)> {
+    // TODO: get user from cookies and days from redis
+    Some((
+        Uuid::parse_str("9ab4649c-9a8e-4fc0-b696-a598327f2a70").ok()?,
+        5,
+    ))
+}
+
+async fn shirts(pool: &PgPool, user_id: Uuid, days: i32) -> sqlx::Result<Vec<ClotheColor>> {
     sqlx::query_as(
         r#"
-        SELECT id FROM ? WHERE user_id = ? AND last_use < now() - INTERVAL ?
+        SELECT
+            color
+        FROM
+            shirts
+        WHERE
+            user_id = $1
+            AND last_use < NOW() - INTERVAL '$1 days'
         "#,
     )
-    .bind(clothes)
     .bind(user_id)
-    .bind(format!("{days} days"))
+    .bind(days)
     .fetch_all(pool)
     .await
 }
 
-async fn shirt(pool: &PgPool, shirts: Vec<ClotheId>) -> sqlx::Result<SvgWithId> {
+async fn shorts(pool: &PgPool, user_id: Uuid, days: i32) -> sqlx::Result<Vec<ClotheColor>> {
+    sqlx::query_as(
+        r#"
+        SELECT
+            color
+        FROM
+            shorts
+        WHERE
+            user_id = $1
+            AND last_use < NOW() - INTERVAL '$1 days'
+        "#,
+    )
+    .bind(user_id)
+    .bind(days)
+    .fetch_all(pool)
+    .await
+}
+
+async fn shirt(shirts: Vec<ClotheColor>) -> Option<ClotheColor> {
     use rand::seq::SliceRandom;
-    let Some(id) = shirts.choose(&mut rand::thread_rng()).map(|x| x.id) else {
-        todo!()
-    };
-
-    sqlx::query_as(
-        r#"
-        SELECT svg FROM shirts WHERE id = ?
-        "#,
-    )
-    .bind(id)
-    .fetch_one(pool)
-    .await
+    shirts.choose(&mut rand::thread_rng()).map(|x| x.clone())
 }
 
-async fn combos(pool: &PgPool, shirt_id: i64) -> sqlx::Result<Vec<WeightedCombo>> {
+async fn combos(
+    pool: &PgPool,
+    user_id: Uuid,
+    ClotheColor { color }: ClotheColor,
+) -> sqlx::Result<Vec<WeightedShort>> {
     sqlx::query_as(
         r#"
-        SELECT short_id FROM combos WHERE shirt_id = ?
+        SELECT 
+            short_color,
+            weight
+        FROM 
+            combos 
+        WHERE 
+            short_user_id = $1
+            AND shirt_color = $2
         "#,
     )
-    .bind(shirt_id)
+    .bind(user_id)
+    .bind(color)
     .fetch_all(pool)
     .await
 }
 
-async fn combo(pool: Arc<PgPool>) -> sqlx::Result<SvgWithId> {
-    // 0. user_id and days
-    let (user_id, days) = user_id_and_days().await;
+async fn combo(pool: Arc<PgPool>) -> Result<impl IntoResponse, Error> {
+    // // 0. user_id and days
+    let (user_id, days) = user_id_and_days()
+        .await
+        .ok_or_else(|| Error::UserIdAndDays)?;
+
     // 1. `spawn` shorts
     let shorts_pool = Arc::clone(&pool);
-    let shorts =
-        tokio::spawn(async move { clothes(shorts_pool.as_ref(), "shorts", user_id, days).await });
-    // 2. `await` shirts
-    let shirts = clothes(pool.as_ref(), "shirts", user_id, days).await?;
+    let shorts = tokio::spawn(async move { shorts(shorts_pool.as_ref(), user_id, days).await });
+
+    // // 2. `await` shirts
+    let shirts = shirts(pool.as_ref(), user_id, days).await?;
+    info!(?shirts);
 
     // 3. `await` shirt
-    let shirt = shirt(pool.as_ref(), shirts).await?;
+    let shirt = shirt(shirts)
+        .await
+        .ok_or_else(|| Error::EmptyClothes("shirts"))?;
+    info!(?shirt);
 
     // 4. `await` combos
-    let combos = combos(pool.as_ref(), shirt.id).await?;
+    let combos = combos(pool.as_ref(), user_id, shirt.clone()).await?;
+    info!(?combos);
 
     // 5. `await` shorts `await` short
-    let shorts = shorts.await.map_err(|_| sqlx::Error::WorkerCrashed)?;
-    todo!()
+    let shorts = shorts.await??;
+    info!(?shorts);
+
+    // the crossover point of linnear seach and a hash map should be crossed
+    let apply_weight = |short_color: i32| WeightedShort {
+        short_color,
+        weight: combos
+            .iter()
+            .find(|x| x.short_color == short_color)
+            .map(|x| x.weight)
+            .unwrap_or(1.0),
+    };
+
+    use rand::distributions::WeightedIndex;
+    use rand::prelude::*;
+
+    let dist = WeightedIndex::new(
+        shorts
+            .iter()
+            .map(|ClotheColor { color }| apply_weight(*color))
+            .map(|WeightedShort { weight, .. }| weight),
+    )
+    .unwrap();
+    info!(?dist);
+
+    let short = shorts[dist.sample(&mut thread_rng())].clone();
+    info!(?short);
+
+    Ok(Html(
+        Combo {
+            short_color: short.color,
+            shirt_color: shirt.color,
+        }
+        .render()?,
+    ))
+}
+
+async fn load(State(pool): State<Arc<PgPool>>) -> impl IntoResponse {
+    combo(pool).await
 }
 
 async fn rating(
     State(pool): State<Arc<PgPool>>,
-    HtmxPostRequest(rated): HtmxPostRequest<RatedCombo>,
+    HtmxPostRequest(RatedCombo {
+        shirt_color,
+        short_color,
+        rating,
+    }): HtmxPostRequest<RatedCombo>,
 ) -> impl IntoResponse {
-    let combo = combo(pool);
-    Html(rated.render().unwrap())
-    // Html(format!(
-    //     r#"
-    //     <h1> rating {rated:?} </h1>
-    //     <input name="shirt_id" type="hidden" value="2"/>
-    //     <input name="short_id" type="hidden" value="2"/>
-    //     <input name="weight" type="hidden" value="1.0"/>
-    //     "#,
-    // ))
-}
-
-async fn load(State(pool): State<Arc<PgPool>>) -> impl IntoResponse {
-    let combo = combo(pool);
-    Html(
-        r#"
-        <h1> load </h1>
-        <input name="shirt_id" type="hidden" value="2"/>
-        <input name="short_id" type="hidden" value="2"/>
-        <input name="weight" type="hidden" value="1.0"/>
-        "#,
-    )
+    info!(?rating, ?shirt_color, ?short_color);
+    // TODO: use raiting
+    combo(pool).await
 }
 
 #[shuttle_runtime::main]
@@ -157,9 +240,13 @@ async fn axum(
     #[shuttle_secrets::Secrets] secret_store: shuttle_secrets::SecretStore,
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
 ) -> shuttle_axum::ShuttleAxum {
-    let pool = PgPool::connect(&secret_store.get("DB_URL").expect("url"))
-        .await
-        .map_err(|e| shuttle_runtime::Error::Database(e.to_string()))?;
+    let pool = PgPool::connect(
+        &secret_store
+            .get("DB_URL")
+            .ok_or_else(|| shuttle_runtime::Error::BuildPanic("Missing DB_URL".to_string()))?,
+    )
+    .await
+    .map_err(|e| shuttle_runtime::Error::Database(e.to_string()))?;
 
     async fn handle_404() -> (StatusCode, &'static str) {
         (StatusCode::NOT_FOUND, "Not found")
